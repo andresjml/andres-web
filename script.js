d638ef363ce4e13fcc6f458cb3361a9db90a77af
Andres <andresjml@gmail.com>
2021-03-27 08:25:01 -0400

    adding const to script.js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-let h1 = document.createElement('h1')
-h1.textContent = "This content added by JavaScript2"
-document.querySelector('body').appendChild(h1)
+const workExp = [
+    {country:"USA", position:"Yard Specialist/Transportation Associate", company: "Amazon.com."}
+    {country:"USA", position:"CAP-Process Assistant", company: "Amazon.com."}
+    {country:"USA", position:"Shipping Clerck", company: "Kendall Toyota."}
+    {country:"Venezuela", position:"Sales Manager", company: "AJM Suministros, C.A."}
+    {country:"Venezuela", position:"Sales Supervisor", company: "Hidroferca Valencia, C.A."}
+    {country:"Venezuela", position:"Routing Analyst", company: "KRAFT Foods Venezuela."}
+]
