ca14877c38fcca6cd74ce78d4053c1d6955ad1ad
Andres <andresjml@gmail.com>
2021-03-27 08:54:04 -0400

    coding in script.js

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 const workExp = [
-    {country:"USA", position:"Yard Specialist/Transportation Associate", company: "Amazon.com."}
-    {country:"USA", position:"CAP-Process Assistant", company: "Amazon.com."}
-    {country:"USA", position:"Shipping Clerck", company: "Kendall Toyota."}
-    {country:"Venezuela", position:"Sales Manager", company: "AJM Suministros, C.A."}
-    {country:"Venezuela", position:"Sales Supervisor", company: "Hidroferca Valencia, C.A."}
-    {country:"Venezuela", position:"Routing Analyst", company: "KRAFT Foods Venezuela."}
-]
+    {line:"Line1", country:"USA", position:"Yard Specialist/Transportation Associate", company: "Amazon.com."}
+    {line:"Line2", country:"USA", position:"CAP-Process Assistant", company: "Amazon.com."}
+    {line:"Line3", country:"USA", position:"Shipping Clerck", company: "Kendall Toyota."}
+    {line:"Line4", country:"Venezuela", position:"Sales Manager", company: "AJM Suministros, C.A."}
+    {line:"Line5", country:"Venezuela", position:"Sales Supervisor", company: "Hidroferca Valencia, C.A."}
+    {line:"Line6", country:"Venezuela", position:"Routing Analyst", company: "KRAFT Foods Venezuela."}
+];
+
+function workLine(workLine) {
+    const workLine = document.createElement("div");
+    workLine.className = "workLine";
+    workLine.innerHTML = 
+    `<h3>${workLine.country}</h3>
+
+
+    <small>population: ${city.population}</small>
+    <p>You'll want to go see the ${city.attraction}</p>
+    `
+    return card
+  }
+
